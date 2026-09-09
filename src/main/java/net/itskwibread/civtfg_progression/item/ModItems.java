8608019fdcad850de047cc92551be824518f08ac
XenomorphickGame <marko@lesoski.mk>
2026-09-09 17:41:52 +0200

    Yup

---
+++ src/main/java/net/itskwibread/civtfg_progression/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.itskwibread.civtfg_progression.item;
+
+import net.itskwibread.civtfg_progression.CivTFG_Progression;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CivTFG_Progression.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
